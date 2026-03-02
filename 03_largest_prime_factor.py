0455a65d8a8eadb4b123828e87728dbb49805893
PiyushKhewalkar <work.piyushkkr@gmail.com>
2026-03-03 00:51:19 +0530

    fix: formatting

--- 03_largest_prime_factor.py
+++ 03_largest_prime_factor.py
@@ -2,8 +2,8 @@ def largest_prime_factor(n):
     d = 2
     largest = None
 
-    while d*d <= n:
-        while n%d == 0:
+    while d * d <= n:
+        while n % d == 0:
             largest = d
             n = n // d
         d += 1
